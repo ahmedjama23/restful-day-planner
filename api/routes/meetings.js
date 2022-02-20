602d23abede907927597b457c5fd98c9000410ae
“Ahmed <ajama07@hotmail.com~>
2022-02-20 15:01:14 -0700

    Adding referential checks for existing agenda items when creating meetings

--- api/routes/meetings.js
+++ api/routes/meetings.js
@@ -3,13 +3,28 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Meeting = require('../models/meetings');
-
+const AgendaItem = require('../models/agendaItems');
 
 router.get('/', (request, response, next) => {
     Meeting.find()
+        .select('-__v')
         .exec()
         .then(docs => {
-            response.status(200).json(docs);
+            response.status(200).json({
+                count: docs.length,
+                meetings: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        title: doc.title,
+                        meetingLength: doc.meetingLength,
+                        agendaItem: doc.agendaItem,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/meetings/' + doc._id
+                        }
+                    }
+                })
+            });
         })
         .catch(error => {
             response.status(500).json({ error: error })
@@ -17,32 +32,64 @@ router.get('/', (request, response, next) => {
 });
 
 router.post('/', (request, response, next) => {
-    const meeting = new Meeting({
-        _id: mongoose.Types.ObjectId(),
-        title: request.body.title,
-        meetingLength: request.body.meetingLength,
-        agendaItem: request.body.agendaItem
-    })
-    meeting.save()
+    AgendaItem.findById(request.body.agendaItem)
+        .then(agendaItem => {
+            if (!agendaItem) {
+                throw new Error("Agenda Item Not Found")
+            }
+            const meeting = new Meeting({
+                _id: mongoose.Types.ObjectId(),
+                title: request.body.title,
+                meetingLength: request.body.meetingLength,
+                agendaItem: request.body.agendaItem
+            })
+            return meeting.save()
+        })
         .then(result => {
             console.log(result);
-            response.status(201).json({ result })
-        })
-        .catch(error => {
-            console.error(error);
-            response.status(500).json({
-                error: error
+            response.status(201).json({
+                message: 'Meeting created',
+                createdMeeting: {
+                    _id: result._id,
+                    title: result.title,
+                    meetingLength: result.meetingLength,
+                    agendaItem: result.agendaItem
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/meetings/' + result._id
+                }
             })
+        })
+        .catch(err => {
+            response.status(500).json({
+                error: err
+            }) 
         })
 });
 
 router.get('/:meetingId', (request, response, next) => {
-    const meetingId = request.params.meetingId
-
-    response.status(200).json({
-        message: "Meeting retrieved using GET requests",
-        meetingId: meetingId
-    });
+    Meeting.findById(request.params.meetingId)
+        .exec()
+        .then(meeting => {
+            if(!meeting) {
+                return response.status(404).json({
+                    message: "Meeting not found"
+                })
+            }
+            response.status(200).json({
+                meeting: meeting,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/meetings'
+                }
+            })
+        })
+        .catch(error => {
+            response.status(500).json({
+                error: error
+            })
+        })
 });
 
 router.delete('/:meetingId', (request, response, next) => {
