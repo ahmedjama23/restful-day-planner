374f5057331bb3420922660967184df2652a60aa
“Ahmed <ajama07@hotmail.com~>
2022-02-20 20:23:18 -0700

    Added user login

--- api/routes/users.js
+++ api/routes/users.js
@@ -49,13 +49,46 @@ router.post('/signup', (request, response, next) => {
 
 });
 
+router.post('/login', (request, response, next) => {
+    User.find({email: request.body.email})
+    .exec()
+    .then(users => {
+        if(users.length < 1) {
+            return response.status(401).json({
+                message: 'Auth failed'
+            })
+        }
+        bcrypt.compare(request.body.password, users[0].password, (error, result) => {
+            if (error) {
+                return response.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            if (result) {
+                return response.status(200).json({
+                    message: 'Auth successful'
+                })
+            }
+            return response.status(401).json({
+                message: 'Auth failed'
+            });
+        })
+    })
+    .catch(error => {
+        console.log(error);
+        response.status(500).json({
+            error: error
+        })
+    })
+})
+
 router.delete('/:userId', (request, response, next) => {
     User.deleteOne({_userId: request.params.userId})
     .exec()
     .then(result => {
         return response.status(200).json({
             message: "User deleted"
-        })
+        });
     })
     .catch(err => {
         return response.status(500).json({
